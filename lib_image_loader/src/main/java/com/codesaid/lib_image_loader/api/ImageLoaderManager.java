28910ccd1f67e600d54c0b0b3845b7d8173802cd
CodeSaid <codesaid526@gmail.com>
2019-11-02 11:37:37 +0800

    为 Notification 设置图片封装

--- lib_image_loader/src/main/java/com/codesaid/lib_image_loader/api/ImageLoaderManager.java
+++ lib_image_loader/src/main/java/com/codesaid/lib_image_loader/api/ImageLoaderManager.java
@@ -1,16 +1,18 @@
 package com.codesaid.lib_image_loader.api;
 
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RemoteViews;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
@@ -18,13 +20,15 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.BitmapTransitionOptions;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.NotificationTarget;
 import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
 import com.bumptech.glide.request.transition.Transition;
 import com.codesaid.lib_image_loader.R;
 import com.codesaid.lib_image_loader.image.Utils;
 
+
 import io.reactivex.Observable;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
@@ -128,6 +132,56 @@ public class ImageLoaderManager {
                                 });
                     }
                 });
+    }
+
+    /**
+     * 为 notification 中的 id 控件加载图片
+     *
+     * @param context         context
+     * @param remoteViews     notification 的布局
+     * @param id              notification 中的 id 控件
+     * @param notification    notification
+     * @param NOTIFICATION_ID notification 的 id
+     * @param url             地址
+     */
+    public void displayImageForNotification(Context context, RemoteViews remoteViews, int id,
+                                            Notification notification, int NOTIFICATION_ID, String url) {
+        displayImageForTarget(context,
+                initNotificationTarget(context, remoteViews, id, notification, NOTIFICATION_ID),
+                url);
+    }
+
+    /**
+     * 创建一个 NotificationTarget
+     *
+     * @param context         context
+     * @param remoteViews     notification 的布局
+     * @param id              notification 中的 id 控件
+     * @param notification    notification
+     * @param NOTIFICATION_ID notification 的 id
+     * @return NotificationTarget 对象
+     */
+    private NotificationTarget initNotificationTarget(Context context, RemoteViews remoteViews,
+                                                      int id, Notification notification, int NOTIFICATION_ID) {
+        return new NotificationTarget(context, id, remoteViews, notification, NOTIFICATION_ID);
+    }
+
+    /**
+     * 为 非 View 加载图片
+     *
+     * @param context context
+     * @param target  target
+     * @param url     地址
+     */
+    @SuppressWarnings("unchecked")
+    private void displayImageForTarget(Context context, Target target, String url) {
+        Glide.with(context)
+                .asBitmap()
+                .load(url)
+                .apply(initCommonRequestOption())
+                .transition(BitmapTransitionOptions.withCrossFade())
+                .fitCenter()
+                .into(target);
     }
 
     @SuppressLint("CheckResult")
