db32e120f1f243eaddc504d63a6745f9c2e88a7f
CodeSaid <codesaid526@gmail.com>
2019-11-05 16:32:02 +0800

    播放相关方法实现

--- lib_audio/src/main/java/com/codesaid/lib_audio/mediaplayer/Test.java
+++
@@ -1,9 +0,0 @@
-package com.codesaid.lib_audio.mediaplayer;
-
-/**
- * Created By codesaid
- * On :2019-11-04
- * Package Name: com.codesaid.lib_audio.mediaplayer
- */
-public class Test {
-}
--- lib_audio/src/main/java/com/codesaid/lib_audio/mediaplayer/core/AudioPlayer.java
+++ lib_audio/src/main/java/com/codesaid/lib_audio/mediaplayer/core/AudioPlayer.java
@@ -8,8 +8,11 @@ import android.net.wifi.WifiManager;
 import android.os.Handler;
 import android.os.Message;
 import android.os.PowerManager;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.codesaid.lib_audio.app.AudioHelper;
+import com.codesaid.lib_audio.mediaplayer.model.AudioBean;
 
 
 /**
@@ -67,6 +70,101 @@ public class AudioPlayer implements MediaPlayer.OnCompletionListener, MediaPlaye
                 .getSystemService(Context.WIFI_SERVICE)).createWifiLock(WifiManager.WIFI_MODE_FULL, TAG);
         // 初始化音频焦点管理器
         mAudioFocusManager = new AudioFocusManager(AudioHelper.getContext(), this);
+    }
+
+    /**
+     * 对外提供的加载方法
+     *
+     * @param audioBean AudioBean
+     */
+    public void load(AudioBean audioBean) {
+        try {
+            // 正常加载逻辑
+            mMediaPlayer.reset();
+            mMediaPlayer.setDataSource(audioBean.mUrl);
+            mMediaPlayer.prepareAsync();
+            // TODO 对外发送 load 事件
+        } catch (Exception e) {
+            // TODO 对外发送 error 事件
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 对外提供的 start 方法
+     */
+    @SuppressLint("ShowToast")
+    public void start() {
+        if (!mAudioFocusManager.requestAudioFocus()) {
+            Log.e(TAG, "获取音频焦点失败");
+            Toast.makeText(AudioHelper.getContext(), "获取音频焦点失败", Toast.LENGTH_SHORT);
+        }
+        mMediaPlayer.start();
+        mWifiLock.acquire();
+
+        // TODO 对外发送 start 事件
+    }
+
+    /**
+     * 对外提供 暂停  方法
+     */
+    public void pause() {
+        if (getStatus() == CustomMediaPlayer.Status.STARTED) {
+            mMediaPlayer.pause();
+
+            // 释放音频焦点 wifiLock
+            if (mWifiLock.isHeld()) {
+                mWifiLock.release();
+            }
+
+            // 释放音频焦点
+            if (mAudioFocusManager != null) {
+                mAudioFocusManager.abandonAudioFocus();
+            }
+
+            // TODO 发送暂停事件
+        }
+    }
+
+    /**
+     * 对外提供 恢复 方法
+     */
+    public void resume() {
+        if (getStatus() == CustomMediaPlayer.Status.STOPPED) {
+            start();
+        }
+    }
+
+    /**
+     * 清空播放器所暂用的资源
+     */
+    public void release() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+            if (mAudioFocusManager != null) {
+                mAudioFocusManager.abandonAudioFocus();
+            }
+            if (mWifiLock.isHeld()) {
+                mWifiLock.release();
+            }
+            mAudioFocusManager = null;
+            mWifiLock = null;
+        }
+
+        // TODO 发送 release 销毁事件
+    }
+
+    /**
+     * 对外提供的  获取 CustomMediaPlayer.Status 状态的方法
+     *
+     * @return 返回 CustomMediaPlayer.Status 状态
+     */
+    public CustomMediaPlayer.Status getStatus() {
+        if (mMediaPlayer != null) {
+            return mMediaPlayer.getStatus();
+        }
+        return CustomMediaPlayer.Status.STOPPED;
     }
 
     @Override
