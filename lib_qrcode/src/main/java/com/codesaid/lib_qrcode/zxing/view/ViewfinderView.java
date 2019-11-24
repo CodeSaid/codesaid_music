246afc1f283345a3be50dc8660544b01f51baf9e
CodeSaid <codesaid526@gmail.com>
2019-11-24 22:39:51 +0800

    自定义View

--- lib_qrcode/src/main/java/com/codesaid/lib_qrcode/zxing/view/ViewfinderView.java
+++ lib_qrcode/src/main/java/com/codesaid/lib_qrcode/zxing/view/ViewfinderView.java
@@ -24,12 +24,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
-import android.hardware.camera2.CameraManager;
 import android.util.AttributeSet;
 import android.view.View;
 
 import com.codesaid.lib_qrcode.R;
+import com.codesaid.lib_qrcode.zxing.camera.CameraManager;
 import com.google.zxing.ResultPoint;
+
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -42,162 +43,163 @@ import java.util.HashSet;
  */
 public final class ViewfinderView extends View {
 
-  private static final int[] SCANNER_ALPHA = {
-      0, 64, 128, 192, 255, 192, 128, 64
-  };
+    private static final int[] SCANNER_ALPHA = {
+            0, 64, 128, 192, 255, 192, 128, 64
+    };
 
-  private final Paint paint;
-  private Bitmap resultBitmap;
-  private final int maskColor;
-  private final int resultColor;
-  private final int resultPointColor;
-  private volatile Collection<ResultPoint> possibleResultPoints;
-  private volatile Collection<ResultPoint> lastPossibleResultPoints;
+    private final Paint paint;
+    private Bitmap resultBitmap;
+    private final int maskColor;
+    private final int resultColor;
+    private final int resultPointColor;
+    private volatile Collection<ResultPoint> possibleResultPoints;
+    private volatile Collection<ResultPoint> lastPossibleResultPoints;
 
-  private static final long ANIMATION_DELAY = 10L;
-  private static final int OPAQUE = 0xFF;
+    private static final long ANIMATION_DELAY = 10L;
+    private static final int OPAQUE = 0xFF;
 
-  private int ScreenRate;
+    private int ScreenRate;
 
-  private static final int CORNER_WIDTH = 8;
+    private static final int CORNER_WIDTH = 8;
 
-  private static final int SPEEN_DISTANCE = 5;
+    private static final int SPEEN_DISTANCE = 5;
 
-  private static float density;
-  private static final int TEXT_SIZE = 13;
+    private static float density;
+    private static final int TEXT_SIZE = 13;
 
-  private static final int TEXT_PADDING_TOP = 30;
+    private static final int TEXT_PADDING_TOP = 30;
 
-  private int slideTop;
-  private int slideBottom;
-  private boolean isFirst;
+    private int slideTop;
+    private int slideBottom;
+    private boolean isFirst;
 
-  // This constructor is used when the class is built from an XML resource.
-  public ViewfinderView(Context context, AttributeSet attrs) {
-    super(context, attrs);
+    // This constructor is used when the class is built from an XML resource.
+    public ViewfinderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-    // Initialize these once for performance rather than calling them every
-    // time in onDraw().
-    paint = new Paint();
-    Resources resources = getResources();
-    maskColor = resources.getColor(R.color.viewfinder_mask);
-    resultColor = resources.getColor(R.color.result_view);
-    resultPointColor = resources.getColor(R.color.possible_result_points);
-    possibleResultPoints = new HashSet<ResultPoint>(5);
-
-    density = context.getResources().getDisplayMetrics().density;
-    ScreenRate = (int) (15 * density);
-  }
-
-  @Override public void onDraw(Canvas canvas) {
-    Rect frame = CameraManager.get().getFramingRect();
-    if (frame == null) {
-      return;
-    }
-
-    if (!isFirst) {
-      isFirst = true;
-      slideTop = frame.top;
-      slideBottom = frame.bottom;
-    }
-    int width = canvas.getWidth();
-    int height = canvas.getHeight();
-
-    // Draw the exterior (i.e. outside the framing rect) darkened
-    paint.setColor(resultBitmap != null ? resultColor : maskColor);
-    canvas.drawRect(0, 0, width, frame.top, paint);
-    canvas.drawRect(0, frame.top, frame.left, frame.bottom + 1, paint);
-    canvas.drawRect(frame.right + 1, frame.top, width, frame.bottom + 1, paint);
-    canvas.drawRect(0, frame.bottom + 1, width, height, paint);
-
-    if (resultBitmap != null) {
-      // Draw the opaque result bitmap over the scanning rectangle
-      paint.setAlpha(OPAQUE);
-      canvas.drawBitmap(resultBitmap, null, frame, paint);
-    } else {
-
-      paint.setColor(getResources().getColor(android.R.color.holo_red_light));
-      canvas.drawRect(frame.left, frame.top, frame.left + ScreenRate, frame.top + CORNER_WIDTH,
-          paint);
-      canvas.drawRect(frame.left, frame.top, frame.left + CORNER_WIDTH, frame.top + ScreenRate,
-          paint);
-      canvas.drawRect(frame.right - ScreenRate, frame.top, frame.right, frame.top + CORNER_WIDTH,
-          paint);
-      canvas.drawRect(frame.right - CORNER_WIDTH, frame.top, frame.right, frame.top + ScreenRate,
-          paint);
-      canvas.drawRect(frame.left, frame.bottom - CORNER_WIDTH, frame.left + ScreenRate,
-          frame.bottom, paint);
-      canvas.drawRect(frame.left, frame.bottom - ScreenRate, frame.left + CORNER_WIDTH,
-          frame.bottom, paint);
-      canvas.drawRect(frame.right - ScreenRate, frame.bottom - CORNER_WIDTH, frame.right,
-          frame.bottom, paint);
-      canvas.drawRect(frame.right - CORNER_WIDTH, frame.bottom - ScreenRate, frame.right,
-          frame.bottom, paint);
-
-      slideTop += SPEEN_DISTANCE;
-      if (slideTop >= frame.bottom) {
-        slideTop = frame.top;
-      }
-      Rect lineRect = new Rect();
-      lineRect.left = frame.left;
-      lineRect.right = frame.right;
-      lineRect.top = slideTop;
-      lineRect.bottom = slideTop + 18;
-      canvas.drawBitmap(((BitmapDrawable) (getResources().getDrawable(R.drawable.fle))).getBitmap(),
-          null, lineRect, paint);
-
-      paint.setColor(Color.WHITE);
-      paint.setTextSize(TEXT_SIZE * density);
-      String text = getResources().getString(R.string.scan_text);
-      float textWidth = paint.measureText(text);
-
-      canvas.drawText(text, (width - textWidth) / 2,
-          (float) (frame.bottom + (float) TEXT_PADDING_TOP * density), paint);
-
-      Collection<ResultPoint> currentPossible = possibleResultPoints;
-      Collection<ResultPoint> currentLast = lastPossibleResultPoints;
-      if (currentPossible.isEmpty()) {
-        lastPossibleResultPoints = null;
-      } else {
+        // Initialize these once for performance rather than calling them every
+        // time in onDraw().
+        paint = new Paint();
+        Resources resources = getResources();
+        maskColor = resources.getColor(R.color.viewfinder_mask);
+        resultColor = resources.getColor(R.color.result_view);
+        resultPointColor = resources.getColor(R.color.possible_result_points);
         possibleResultPoints = new HashSet<ResultPoint>(5);
-        lastPossibleResultPoints = currentPossible;
-        paint.setAlpha(OPAQUE);
-        paint.setColor(resultPointColor);
-        for (ResultPoint point : currentPossible) {
-          canvas.drawCircle(frame.left + point.getX(), frame.top + point.getY(), 6.0f, paint);
-        }
-      }
-      if (currentLast != null) {
-        paint.setAlpha(OPAQUE / 2);
-        paint.setColor(resultPointColor);
-        for (ResultPoint point : currentLast) {
-          canvas.drawCircle(frame.left + point.getX(), frame.top + point.getY(), 3.0f, paint);
-        }
-      }
 
-      postInvalidateDelayed(ANIMATION_DELAY, frame.left, frame.top, frame.right, frame.bottom);
+        density = context.getResources().getDisplayMetrics().density;
+        ScreenRate = (int) (15 * density);
     }
-  }
 
-  public void drawViewfinder() {
-    resultBitmap = null;
-    invalidate();
-  }
+    @Override
+    public void onDraw(Canvas canvas) {
+        Rect frame = CameraManager.get().getFramingRect();
+        if (frame == null) {
+            return;
+        }
 
-  /**
-   * Draw a bitmap with the result points highlighted instead of the live
-   * scanning display.
-   *
-   * @param barcode An image of the decoded barcode.
-   */
-  public void drawResultBitmap(Bitmap barcode) {
-    resultBitmap = barcode;
-    invalidate();
-  }
+        if (!isFirst) {
+            isFirst = true;
+            slideTop = frame.top;
+            slideBottom = frame.bottom;
+        }
+        int width = canvas.getWidth();
+        int height = canvas.getHeight();
 
-  public void addPossibleResultPoint(ResultPoint point) {
-    synchronized (possibleResultPoints) {
-      possibleResultPoints.add(point);
+        // Draw the exterior (i.e. outside the framing rect) darkened
+        paint.setColor(resultBitmap != null ? resultColor : maskColor);
+        canvas.drawRect(0, 0, width, frame.top, paint);
+        canvas.drawRect(0, frame.top, frame.left, frame.bottom + 1, paint);
+        canvas.drawRect(frame.right + 1, frame.top, width, frame.bottom + 1, paint);
+        canvas.drawRect(0, frame.bottom + 1, width, height, paint);
+
+        if (resultBitmap != null) {
+            // Draw the opaque result bitmap over the scanning rectangle
+            paint.setAlpha(OPAQUE);
+            canvas.drawBitmap(resultBitmap, null, frame, paint);
+        } else {
+
+            paint.setColor(getResources().getColor(android.R.color.holo_red_light));
+            canvas.drawRect(frame.left, frame.top, frame.left + ScreenRate, frame.top + CORNER_WIDTH,
+                    paint);
+            canvas.drawRect(frame.left, frame.top, frame.left + CORNER_WIDTH, frame.top + ScreenRate,
+                    paint);
+            canvas.drawRect(frame.right - ScreenRate, frame.top, frame.right, frame.top + CORNER_WIDTH,
+                    paint);
+            canvas.drawRect(frame.right - CORNER_WIDTH, frame.top, frame.right, frame.top + ScreenRate,
+                    paint);
+            canvas.drawRect(frame.left, frame.bottom - CORNER_WIDTH, frame.left + ScreenRate,
+                    frame.bottom, paint);
+            canvas.drawRect(frame.left, frame.bottom - ScreenRate, frame.left + CORNER_WIDTH,
+                    frame.bottom, paint);
+            canvas.drawRect(frame.right - ScreenRate, frame.bottom - CORNER_WIDTH, frame.right,
+                    frame.bottom, paint);
+            canvas.drawRect(frame.right - CORNER_WIDTH, frame.bottom - ScreenRate, frame.right,
+                    frame.bottom, paint);
+
+            slideTop += SPEEN_DISTANCE;
+            if (slideTop >= frame.bottom) {
+                slideTop = frame.top;
+            }
+            Rect lineRect = new Rect();
+            lineRect.left = frame.left;
+            lineRect.right = frame.right;
+            lineRect.top = slideTop;
+            lineRect.bottom = slideTop + 18;
+            canvas.drawBitmap(((BitmapDrawable) (getResources().getDrawable(R.drawable.fle))).getBitmap(),
+                    null, lineRect, paint);
+
+            paint.setColor(Color.WHITE);
+            paint.setTextSize(TEXT_SIZE * density);
+            String text = getResources().getString(R.string.scan_text);
+            float textWidth = paint.measureText(text);
+
+            canvas.drawText(text, (width - textWidth) / 2,
+                    (float) (frame.bottom + (float) TEXT_PADDING_TOP * density), paint);
+
+            Collection<ResultPoint> currentPossible = possibleResultPoints;
+            Collection<ResultPoint> currentLast = lastPossibleResultPoints;
+            if (currentPossible.isEmpty()) {
+                lastPossibleResultPoints = null;
+            } else {
+                possibleResultPoints = new HashSet<ResultPoint>(5);
+                lastPossibleResultPoints = currentPossible;
+                paint.setAlpha(OPAQUE);
+                paint.setColor(resultPointColor);
+                for (ResultPoint point : currentPossible) {
+                    canvas.drawCircle(frame.left + point.getX(), frame.top + point.getY(), 6.0f, paint);
+                }
+            }
+            if (currentLast != null) {
+                paint.setAlpha(OPAQUE / 2);
+                paint.setColor(resultPointColor);
+                for (ResultPoint point : currentLast) {
+                    canvas.drawCircle(frame.left + point.getX(), frame.top + point.getY(), 3.0f, paint);
+                }
+            }
+
+            postInvalidateDelayed(ANIMATION_DELAY, frame.left, frame.top, frame.right, frame.bottom);
+        }
     }
-  }
+
+    public void drawViewfinder() {
+        resultBitmap = null;
+        invalidate();
+    }
+
+    /**
+     * Draw a bitmap with the result points highlighted instead of the live
+     * scanning display.
+     *
+     * @param barcode An image of the decoded barcode.
+     */
+    public void drawResultBitmap(Bitmap barcode) {
+        resultBitmap = barcode;
+        invalidate();
+    }
+
+    public void addPossibleResultPoint(ResultPoint point) {
+        synchronized (possibleResultPoints) {
+            possibleResultPoints.add(point);
+        }
+    }
 }
