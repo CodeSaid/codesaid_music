f80fd38bc5942f7372f7578e67a154335607491b
CodeSaid <codesaid526@gmail.com>
2019-11-26 17:32:37 +0800

    BaseActivity 封装

--- lib_commin_ui/src/main/java/com/codesaid/lib_commin_ui/base/BaseActivity.java
+++ lib_commin_ui/src/main/java/com/codesaid/lib_commin_ui/base/BaseActivity.java
@@ -1,10 +1,14 @@
 package com.codesaid.lib_commin_ui.base;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.codesaid.lib_commin_ui.base.constant.Constant;
 import com.codesaid.lib_commin_ui.utils.StatusBarUtil;
 
 /**
@@ -17,6 +21,51 @@ public class BaseActivity extends FragmentActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarUtil.statusBarLightMode(this);
+    }
 
+    /**
+     * 申请指定的权限.
+     */
+    public void requestPermission(int code, String... permissions) {
+        ActivityCompat.requestPermissions(this, permissions, code);
+    }
+
+    /**
+     * 判断是否有指定的权限
+     */
+    public boolean hasPermission(String... permissions) {
+
+        for (String permisson : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permisson) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case Constant.WRITE_READ_EXTERNAL_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doSDCardPermission();
+                }
+                break;
+            case Constant.HARDWEAR_CAMERA_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    doCameraPermission();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 处理整个应用用中的SDCard业务
+     */
+    public void doSDCardPermission() {
+    }
+
+    public void doCameraPermission() {
     }
 }
