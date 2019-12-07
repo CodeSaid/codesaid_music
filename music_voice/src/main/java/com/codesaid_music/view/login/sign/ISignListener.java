012e6897ee5599d729b54d34d2f42cc2f8029d4a
CodeSaid <codesaid526@gmail.com>
2019-12-07 23:29:47 +0800

    登录监听Listener

---
+++ music_voice/src/main/java/com/codesaid_music/view/login/sign/ISignListener.java
@@ -0,0 +1,12 @@
+package com.codesaid_music.view.login.sign;
+
+/**
+ * Created By codesaid
+ * On :2019-12-07
+ * Package Name: com.codesaid_music.view.login.sign
+ */
+public interface ISignListener {
+    void onSignInSuccess();
+
+    void onSignUpSuccess();
+}
