38fcac52dd2ce3916cfec8187165453e6d4a0874
CodeSaid <codesaid526@gmail.com>
2019-12-10 11:45:06 +0800

    register init

--- music_voice/src/main/java/com/codesaid_music/view/login/LoginActivity.java
+++ music_voice/src/main/java/com/codesaid_music/view/login/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
@@ -20,7 +21,7 @@ import com.google.android.material.textfield.TextInputEditText;
  * Package Name: com.codesaid_music.view.login
  * desc: 登录页面
  */
-public class LoginActivity extends BaseActivity implements IUserLoginView {
+public class LoginActivity extends BaseActivity implements IUserLoginView, View.OnClickListener {
 
     private UserLoginPresenter mUserLoginPresenter;
 
@@ -29,6 +30,8 @@ public class LoginActivity extends BaseActivity implements IUserLoginView {
 
     private String email;
     private String password;
+    private TextView mTvRegister;
+    private TextView mLoginView;
 
 
     public static void start(Context context) {
@@ -42,20 +45,21 @@ public class LoginActivity extends BaseActivity implements IUserLoginView {
         setContentView(R.layout.activity_login_layout);
         mEmail = findViewById(R.id.edit_sign_in_email);
         mEmail = findViewById(R.id.edit_sign_in_password);
+        mTvRegister = findViewById(R.id.tv_register);
+        mLoginView = findViewById(R.id.login_view);
+
+        mTvRegister.setOnClickListener(this);
+        mLoginView.setOnClickListener(this);
 
         //初始化P层
         mUserLoginPresenter = new UserLoginPresenter(this);
-
-        findViewById(R.id.login_view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (checkForm()) {
-                    mUserLoginPresenter.login(email, password);
-                }
-            }
-        });
     }
 
+    /**
+     * 判断账号面膜格式
+     *
+     * @return true
+     */
     private boolean checkForm() {
         email = mEmail.getText().toString().trim();
         password = mPassword.getText().toString().trim();
@@ -108,5 +112,19 @@ public class LoginActivity extends BaseActivity implements IUserLoginView {
     @Override
     public void hideLoadingView() {
         //隐藏加载布局
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.tv_register:
+                LoginActivity.this.startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                break;
+            case R.id.login_view:
+                if (checkForm()) {
+                    mUserLoginPresenter.login(email, password);
+                }
+                break;
+        }
     }
 }
---
+++ music_voice/src/main/java/com/codesaid_music/view/login/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.codesaid_music.view.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.codesaid.lib_commin_ui.base.BaseActivity;
+import com.codesaid_music.R;
+
+/**
+ * Created By codesaid
+ * On :2019-12-10
+ * Package Name: com.codesaid_music.view.login
+ */
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_layout);
+    }
+}
